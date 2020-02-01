73072c0d5cb05f7e2100d1c14335379e6d406bda
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-02-01 01:40:56 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # Build your say_hello method here
-def say_hello(Gabriela = "Ruby Programmer!")
- puts "Hello #{Gabriela!}"
+def say_hello(name = "Ruby Programmer!")
+  print "Hello #{{name!}}"
 end
 
-say_hello Gabriela
+say_hello name
