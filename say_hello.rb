305ea56ea45db890999796ef15ce3e68889b5050
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-02-01 02:21:50 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,10 @@
 # Build your say_hello method here
-def say_hello(name = "Ruby Programmer")
-  print "Hello #{name}!"
+def say_hello(name)
+  puts "Hello Kent Beck"
+end
+
+def say_hello(name ="Ruby Programmer!")
+  print ("Hello #{name}!")
 end
 
 say_hello("name")
